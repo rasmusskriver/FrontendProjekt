85c8a908d6de176b4f8159c26f1d9847d4588353
rasmusskriver <rasmus_skriver@yahoo.dk>
2024-09-07 00:24:11 +0200

    cors er dejligt

---
+++ app.js
@@ -0,0 +1,29 @@
+const myImage = document.querySelector("img");
+
+const myRequest = new Request("flowers.jpg");
+
+function buttonFunction() {
+
+	fetch(myRequest)
+
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			return response.blob();
+		})
+
+		.then((response) => {
+			myImage.src = URL.createObjectURL(response);
+
+		})
+};
+
+// document.getElementById("tekstboks").innerHTML="Vellevois et eller andet lort på fransk";
+
+function resetFunction() {
+
+	document.getElementById("tekstboks").innerHTML="";
+
+};
