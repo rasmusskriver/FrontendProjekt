4101a0b259e6d7a528dec7488e96dce1e561b82c
rasmusskriver <rasmus_skriver@yahoo.dk>
2024-09-07 00:50:56 +0200

    Console log, ja det driller sku lidt

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const myImage = document.querySelector("img");
 
 const myRequest = new Request("flowers.jpg");
 
+console.log(myRequest.url);
+console.log(myRequest.method);
+console.log(myRequest.headers);
+console.log(myRequest.body);
+
+
 function buttonFunction() {
 
 	fetch(myRequest)
 
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
 
-			return response.blob();
-		})
+		return response.blob();
+	})
 
 		.then((response) => {
 			myImage.src = URL.createObjectURL(response);
